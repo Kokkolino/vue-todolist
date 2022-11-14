002450e11ba971aa81571215794e73881442fddc
Francesco C <fran461j@outlook.it>
2022-11-14 17:12:39 +0100

    everything works

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,8 +16,27 @@ var app = new Vue({
         return this.todoElements.push(x);
       },
       // delete task
-      deleteTask: function (position) {
-        return this.todoElements.splice(position,1)
+      deleteTask: function (elem, position) {
+        if(elem.done === false){
+          return this.todoElements.splice(position,1)
+        }
+        else{
+          return this.completedTasks.splice(position,1)
+        }
+      },
+      checkTask: function (elem, position) {
+        if(elem.done === false){
+          const x = this.todoElements[position];
+          x.done = true;
+          this.todoElements.splice(position,1)
+          return this.completedTasks.push(x)
+        }
+        else{
+          const x = this.completedTasks[position];
+          x.done = false;
+          this.completedTasks.splice(position,1)
+          return this.todoElements.push(x)
+        }
       }
     }
   })
